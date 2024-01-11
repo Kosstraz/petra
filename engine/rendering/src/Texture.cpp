0bf8fb9c64c2ce077c520a2e40ad2179c2c9d22b
Kosstraz <60485785+Kosstraz@users.noreply.github.com>
2024-01-11 15:51:19 +0100

    Add files via upload

--- engine/rendering/src/Texture.cpp
+++ engine/rendering/src/Texture.cpp
@@ -10,9 +10,9 @@ Texture::~Texture() noexcept
 
 void Texture::CreateTexture(const char* image_dir) noexcept
 {
-    void* datas;
-    BMP_infos info = bmp_load(image_dir, &datas);
-    
+    unsigned char* data = ((unsigned char*)0);
+    BMP_infos info = bmp_load(image_dir, &data);
+
     if (info.errorCode < 0)
     {
         DEBUG(TEXTURE_LOG, "Erreur dans 'CreateTexture(...)' avec 'bmp_load'")
@@ -26,14 +26,18 @@ void Texture::CreateTexture(const char* image_dir) noexcept
         glGenTextures(1, &this->textureID);
         glBindTexture(GL_TEXTURE_2D, this->textureID);
         if (info.bitsPerPixel == 24)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,  this->width, this->height, 0, GL_BGR,  GL_UNSIGNED_BYTE, datas);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,  info.width, info.height, 0, GL_RGB,  GL_UNSIGNED_BYTE, data);
         else
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this->height, 0, GL_BGRA, GL_UNSIGNED_BYTE, datas);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, info.width, info.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         printf("-> nice\n");
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
     }
 
-    free(datas);
+    free(data);
 }
