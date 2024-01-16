802dacc1d1f86530d20ba7c837fd2eda611b44b4
Kosstraz <60485785+Kosstraz@users.noreply.github.com>
2024-01-16 20:26:14 +0100

    Delete mpb.c

--- mpb.c
+++
@@ -1,6 +0,0 @@
-// !'V773 The 'p' pointer was not released in destructor. A memory leak is possible. ptr.inl 18'
-
-// !'V1053 Calling the 'Destroy' virtual function in the destructor may lead to unexpected result at runtime. Camera.cpp 23'
-
-// !V814 Decreased performance. The 'strlen' function was called multiple times inside the body of a loop. glad.c 853
-
